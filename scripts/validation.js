17b2eed098972d952a59d4225f4f375480b8d4dd
naashchaa <maks.maxistmin@yandex.ru>
2023-01-20 16:58:22 +0100

    styling and format changes

---
+++ scripts/validation.js
@@ -0,0 +1,29 @@
+function validate_userid() {
+    let input_field = document.getElementById("id-input");
+    let input_text = input_field.value;
+
+    let length_error = document.getElementById("length-error");
+    let format_error = document.getElementById("format-error");
+
+    if (!check_length(input_text)) {
+        length_error.style.display = "inline-block";
+    }
+    else {
+        length_error.style.display = "none";
+    }
+
+    if (!check_format(input_text)) {
+        format_error.style.display = "inline-block";
+    }
+    else {
+        format_error.style.display = "none";
+    }
+}
+
+function check_length(input) {
+    return input.length > 4 && input.length < 13;
+}
+
+function check_format(input) {
+    // TODO
+}
