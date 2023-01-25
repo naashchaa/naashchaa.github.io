be1d0a2965d4b11854f465c3d511b441418e4103
ksenia <kseniasycheva0022@gmail.com>
2023-01-25 01:59:49 +0100

    option 1 fixed

--- assn2 method 2/scripts/validation.js
+++ assn2 method 2/scripts/validation.js
@@ -73,3 +73,28 @@ function check_zipcode_format(input) {
     return pattern.test(input);
 }
 
+function validate_name() {
+    let name = document.getElementById("name-input").value;
+    let error = document.getElementById("name-error");
+
+    const pattern = /[^a-zA-Z]/;
+    if (pattern.exec(name)) {
+        error.style.display = "inline-block";
+    }
+    else {
+        error.style.display = "none";
+    }
+}
+
+function validate_surname() {
+    let surname = document.getElementById("surname-input").value;
+    let error = document.getElementById("surname-error");
+
+    const pattern = /[^a-zA-Z]/;
+    if (pattern.exec(surname)) {
+        error.style.display = "inline-block";
+    }
+    else {
+        error.style.display = "none";
+    }
+}
